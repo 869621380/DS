a02a2de1495874d65bed006649650a7bf849715c
869621380 <869621380@qq.com>
2024-06-28 18:43:37 +0800

    Stack and StackApplication

--- src/t4.java
+++ src/t4.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
+
 public class t4 {
     public static void main(String[]args){
         Integer []L1={1,3,5,7,9,12};
@@ -9,7 +10,7 @@ public class t4 {
         ArrayList<Integer> L=(ArrayList<Integer>) intersection(Arrays.asList(L1),Arrays.asList(L2));
         System.out.println(L);
     }
-    public static Collection<Integer> intersection(Collection<Integer>L1,Collection<Integer>L2) {
+    public static Collection<Integer> intersection(Collection<Integer>L1, Collection<Integer>L2) {
         Iterator<Integer> iter1 = L1.iterator();
         Iterator<Integer> iter2 = L2.iterator();
         ArrayList<Integer> L = new ArrayList<>();
@@ -23,7 +24,6 @@ public class t4 {
                 }
                 if (i1 == i2) L.add(i1);
             }
-
         }
         return L;
     }
